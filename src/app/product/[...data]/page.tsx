abcb478145091a5c8938ea60126f379818db5740
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-11-15 01:09:20 -0300

    testing server components

--- src/app/product/[...data]/page.tsx
+++ src/app/product/[...data]/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useCallback } from "react";
+
 interface ProductProps {
   params: {
     data: string[];
@@ -6,11 +10,16 @@ interface ProductProps {
 
 export default function ProductPage(props: ProductProps) {
   const [productId, size, color] = props.params.data;
+
+  const onClick = useCallback(() => console.log(`cliquou`), []);
+
   return (
     <>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button onClick={onClick}>Clique me</button>
     </>
   );
 }
